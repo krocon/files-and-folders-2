e04c49f9ad8d061e1cf0ff322c0f909ce5c94287
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2025-07-04 08:44:21 +0200

    Refactor focus and selection local storage services:
    - Updated `SelectionLocalStorage` and `FocusLocalStorage` to use separate `TypedDataService` instances for each panel.
    - Simplified data structures and removed redundant indexing logic.
    - Enhanced consistency and readability of persist, apply, and clear methods across both services.

--- apps/fnf/src/app/component/main/filetable/focus-local-storage.ts
+++ apps/fnf/src/app/component/main/filetable/focus-local-storage.ts
@@ -3,10 +3,7 @@ import {TypedDataService} from "../../../common/typed-data.service";
 import {Injectable} from "@angular/core";
 import {FileTableBodyModel} from "./file-table-body-model";
 
-export type FocusLocalStorageDataType = [
-  { [key: string]: Partial<FileItemIf> },
-  { [key: string]: Partial<FileItemIf> }
-];
+export type FocusLocalStorageDataType = { [key: string]: Partial<FileItemIf> };
 
 
 @Injectable({
@@ -14,7 +11,11 @@ export type FocusLocalStorageDataType = [
 })
 export class FocusLocalStorage {
 
-  private readonly innerService = new TypedDataService<FocusLocalStorageDataType>("focusData", [{}, {}]);
+
+  private readonly innerServices = [
+    new TypedDataService<FocusLocalStorageDataType>("focus0", {}),
+    new TypedDataService<FocusLocalStorageDataType>("focus1", {})
+  ];
 
 
   persistFocus<T>(panelIndex: PanelIndex, path: string, fileTableBodyModel: FileTableBodyModel) {
@@ -22,28 +23,33 @@ export class FocusLocalStorage {
     this.persistFocusCriteria(panelIndex, path, criteria);
   }
 
-  persistFocusCriteria<T>(panelIndex: PanelIndex, path: string, criteria: Partial<FileItemIf>|null|undefined) {
+  persistFocusCriteria<T>(panelIndex: PanelIndex, path: string, criteria: Partial<FileItemIf> | null | undefined) {
+    const innerService = this.getInnerService(panelIndex);
     if (criteria) {
-      const data: FocusLocalStorageDataType = this.innerService.getValue();
-      data[panelIndex][path] = criteria;
-      this.innerService.update(data);
+      const data: FocusLocalStorageDataType = innerService.getValue();
+      data[path] = criteria;
+      innerService.update(data);
 
     } else {
-      const data: FocusLocalStorageDataType = this.innerService.getValue();
-      delete data[panelIndex][path];
-      this.innerService.update(data);
+      const data: FocusLocalStorageDataType = innerService.getValue();
+      delete data[path];
+      innerService.update(data);
     }
   }
 
   applyFocus<T>(panelIndex: PanelIndex, path: string, fileTableBodyModel: FileTableBodyModel) {
-    const data: FocusLocalStorageDataType = this.innerService.getValue();
-    const criteria: Partial<FileItemIf> | undefined = data[panelIndex][path];
+    const data: FocusLocalStorageDataType = this.getInnerService(panelIndex).getValue();
+    const criteria: Partial<FileItemIf> | undefined = data[path];
     fileTableBodyModel.setFocusByCriteria(criteria);
   }
 
   clearAll(): void {
-    this.innerService.update([{}, {}]);
+    this.getInnerService(0).update({});
+    this.getInnerService(1).update({});
   }
 
+  private getInnerService(panelIndex: PanelIndex): TypedDataService<FocusLocalStorageDataType> {
+    return this.innerServices[panelIndex];
+  }
 
 }
--- apps/fnf/src/app/component/main/filetable/selection-local-storage.ts
+++ apps/fnf/src/app/component/main/filetable/selection-local-storage.ts
@@ -4,11 +4,7 @@ import {PanelIndex} from "@fnf/fnf-data";
 import {TypedDataService} from "../../../common/typed-data.service";
 import {Injectable} from "@angular/core";
 
-export type SelectionLocalStorageDataType = [
-  { [key: string]: string[] },
-  { [key: string]: string[] }
-];
-
+export type SelectionLocalStorageDataType = { [key: string]: string[] };
 
 
 @Injectable({
@@ -16,37 +12,45 @@ export type SelectionLocalStorageDataType = [
 })
 export class SelectionLocalStorage {
 
-  private readonly innerService = new TypedDataService<SelectionLocalStorageDataType>("selections", [{}, {}]);
+  private readonly innerServices = [
+    new TypedDataService<SelectionLocalStorageDataType>("selection0", {}),
+    new TypedDataService<SelectionLocalStorageDataType>("selection1", {})
+  ];
 
 
   persistSelection<T>(panelIndex: PanelIndex, path: string, selectionManager: SelectionManagerForObjectModels<T>) {
     const selected: T[] = selectionManager.getSelectedRows();
-
+    const innerService = this.getInnerService(panelIndex);
     if (selected?.length) {
       const value = selected.map((row: T) => selectionManager.options.getKey(row));
 
-      const data: SelectionLocalStorageDataType = this.innerService.getValue();
-      data[panelIndex][path] = value;
-      this.innerService.update(data);
+      const data: SelectionLocalStorageDataType = innerService.getValue();
+      data[path] = value;
+      innerService.update(data);
 
     } else {
-      const data: SelectionLocalStorageDataType = this.innerService.getValue();
-      delete data[panelIndex][path];
-      this.innerService.update(data);
+      const data: SelectionLocalStorageDataType = innerService.getValue();
+      delete data[path];
+      innerService.update(data);
     }
   }
 
   applySelection<T>(panelIndex: PanelIndex, path: string, selectionManager: SelectionManagerForObjectModels<T>) {
-    const data: SelectionLocalStorageDataType = this.innerService.getValue();
-    const selected: any[]|undefined = data[panelIndex][path];
+    const data: SelectionLocalStorageDataType = this.getInnerService(panelIndex).getValue();
+    const selected: any[] | undefined = data[path];
     if (selected) {
       selectionManager.applySelection2Model(selected);
     }
   }
 
   clearAll(): void {
-    this.innerService.update([{}, {}]);
+    this.getInnerService(0).update({});
+    this.getInnerService(1).update({});
   }
 
+
+  private getInnerService(panelIndex: PanelIndex): TypedDataService<SelectionLocalStorageDataType> {
+    return this.innerServices[panelIndex];
+  }
 
 }
