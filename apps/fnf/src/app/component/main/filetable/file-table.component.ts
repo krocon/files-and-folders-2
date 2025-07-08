1f6a076316860a6130a58a4c154bb0f36b26cc2f
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2025-07-08 17:02:22 +0200

    Restrict `mkdir` button when in directories starting with "tabfind":
    - Updated `getButtonEnableStates` to disable the `mkdir` button if `dirPara.path` starts with "tabfind".

--- apps/fnf/src/app/component/main/filetable/file-table.component.ts
+++ apps/fnf/src/app/component/main/filetable/file-table.component.ts
@@ -284,11 +284,12 @@ export class FileTableComponent implements OnInit, OnDestroy, AfterViewInit {
 
   getButtonEnableStates(items: FileItemIf[]): ButtonEnableStates {
     const states = new ButtonEnableStates();
+
     states.copy = !!items?.length && !items[0].dir?.match(EXP_ZIP_FILE_URL);
     states.edit = items?.length === 1 && !items[0].dir?.match(EXP_ZIP_FILE_URL);
     states.move = !!items?.length && !items[0].dir?.match(EXP_ZIP_FILE_URL);
     states.remove = !!items?.length && !items[0].dir?.match(EXP_ZIP_FILE_URL);
-    states.mkdir = true;
+    states.mkdir = !this.dirPara?.path.startsWith('tabfind');
     states.rename = items?.length === 1 && !items[0].dir?.match(EXP_ZIP_FILE_URL);
     states.unpack = items?.length === 1 && !items[0].dir?.match(EXP_ZIP_FILE_URL);
     return states;
