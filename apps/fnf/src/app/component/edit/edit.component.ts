30a09f670e922de91fb39a8394711eaf080e35e1
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2025-07-18 20:40:51 +0200

    wip: monaco editor

--- apps/fnf/src/app/component/edit/edit.component.ts
+++ apps/fnf/src/app/component/edit/edit.component.ts
@@ -1,31 +1,51 @@
-import {ChangeDetectionStrategy, Component} from "@angular/core";
-import {EditorComponent} from "ngx-monaco-editor-v2";
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, NgZone, OnInit} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: "fnf-edit",
-  template: `
-
-    <ngx-monaco-editor
-        class="ngx-monaco-editor"
-        [options]="editorOptions"
-        [(ngModel)]="text"></ngx-monaco-editor>
-  `,
-  styles: [`
-      .ngx-monaco-editor {
-          width: 100vw;
-          height: 100vh;
-      }
-  `],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: "./edit.component.html",
+  styleUrls: ['edit.component.css'],
   imports: [
-    EditorComponent,
     FormsModule
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EditComponent {
+export class EditComponent implements OnInit, AfterViewInit {
 
   editorOptions = {theme: 'vs-dark', language: 'text'};
   text: string = 'function x() {\n\tconsole.log("Hello world!");\n}';
+
+  constructor(
+    private readonly cdr: ChangeDetectorRef,
+    private readonly zone: NgZone,
+  ) {
+  }
+
+  ngAfterViewInit(): void {
+    this.zone.runOutsideAngular(() => {
+      const domElement = document.querySelector(".monaco-editor");
+      const options = {
+        value: "// First line\nfunction hello() {\n\talert('Hello world!');\n}\n// Last line",
+        language: "text",
+
+        lineNumbers: "off",
+        roundedSelection: false,
+        scrollBeyondLastLine: false,
+        readOnly: false,
+        theme: "vs-dark",
+      };
+      const editor = monaco.editor.create(domElement, options);
+      setTimeout(function () {
+        editor.updateOptions({
+          lineNumbers: "on",
+        });
+      }, 2000);
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
+
 
 }
